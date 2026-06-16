1d648fedcb6dc49e282de7e173ceb1780326281a
Jagriti <ruchigiri88@gmail.com>
2026-06-16 20:12:37 +0530

    Added sum of two numbers using functions
    sum of two numbers using double variable

---
+++ function2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//sum of two numbers
+//define function
+double sum( double a, double b){
+    double s= a+b;
+    return s;
+}
+//function call/invoke
+int main(){
+    cout<< sum (10.99,11.42) <<endl;
+return 0;
+}
