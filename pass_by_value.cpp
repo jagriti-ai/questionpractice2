e873c901e95c4c1db199f7814a81cfb044e95862
Jagriti <ruchigiri88@gmail.com>
2026-06-19 20:42:58 +0530

    Refactor changeX function to use void return type

--- pass_by_value.cpp
+++ pass_by_value.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int changeX(int x){
+void changeX(int x){
      x = 2*x;
-     cout << "x = " << x<< endl; //output 10
+     cout << "x = " << x<< endl; // output 10
 
 }
 int main(){
     int x=5;
-    int changeX(x);
+    changeX(x);
 
-    cout<< "x = "<< x<< endl; //final output 5
+    cout<< "x = "<< x<< endl; // output 5
     return 0;
 }
